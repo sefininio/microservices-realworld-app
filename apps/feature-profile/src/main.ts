511807ff79d2fb9aaeb667909833a4eaf0c2f94c
Sefi Ninio <sefi.ninio@gmail.com>
2021-10-11 14:39:27 +0300

    add enableCors to feature services

--- apps/feature-profile/src/main.ts
+++ apps/feature-profile/src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3336;
 
   const config = new DocumentBuilder()
--- apps/feature-user/src/main.ts
+++ apps/feature-user/src/main.ts
@@ -12,6 +12,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3334;
